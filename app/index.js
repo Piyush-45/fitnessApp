a8fbbdb0f157598e23e149a6c93dbcfba216cd78
Piyush Tyagi <tyagipiyush2233@gmail.com>
2024-01-27 13:15:04 +0530

    first commit

---
+++ app/index.js
@@ -0,0 +1,51 @@
+import { View, Text } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { Image } from 'react-native'
+import React from 'react'
+// import Header from '../components/header'
+import Animated from 'react-native-reanimated'
+import { LinearGradient } from "expo-linear-gradient"
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen'
+import { TouchableOpacity } from 'react-native'
+import { FadeInDown } from 'react-native-reanimated'
+import { useRouter } from 'expo-router'
+const index = () => {
+  const router = useRouter()
+  return (
+    <View className='flex-1 flex justify-end' >
+      <StatusBar style="light" />
+      <Image className='h-full w-full  absolute' source={require('../assets/images/welcome.png')} />
+
+      <LinearGradient
+        colors={['transparent', '#18181b']}
+        style={{ width: wp(100), height: hp(70) }}
+        start={{ x: 0.5, y: 0 }}
+        end={{ x: 0.5, y: 0.8 }}
+        className="flex justify-end pb-12 space-y-8">
+
+        <Animated.View entering={FadeInDown.delay(100).springify()} className="flex items-center">
+          <Text style={{ fontSize: hp(5) }} className="text-white font-bold tracking-wide">
+            Best <Text className="text-rose-500">Workouts</Text>
+          </Text>
+          <Text style={{ fontSize: hp(5) }} className="text-white font-bold tracking-wide">
+            For You
+          </Text>
+        </Animated.View>
+
+        <Animated.View entering={FadeInDown.delay(300).springify()}>
+          <TouchableOpacity
+           onPress={()=>router.push('home')}
+            style={{ height: hp(7), width: wp(80) }}
+            className="bg-rose-500 flex items-center justify-center mx-auto rounded-full border-[2px] border-neutral-200"
+          >
+            <Text style={{ fontSize: hp(3) }} className="text-white font-bold tracking-widest">
+              Get Started
+            </Text>
+          </TouchableOpacity>
+        </Animated.View>
+      </LinearGradient>
+    </View>
+  )
+}
+
+export default index
